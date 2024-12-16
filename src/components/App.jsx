afe744d96ef3cde1cb7589671a04f5c7abfcd24b
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2024-12-16 20:23:27 +0100

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,14 @@
-import { Counter } from "./Counter/Counter";
-import { ColorPicker } from "./ColorPicker/ColorPicker";
-import { TodoList } from "./TodoList/TodoList.jsx";
-import { useState } from "react";
-import Modal from "./Modal/Modal.jsx";
-import Vote from "./Vote/Vote.jsx";
+import Options from "./Options/Options.jsx";
+import Description from "./Description/Description.jsx";
+import Feedback from "./Feedback/Feedback.jsx";
 
 const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
   return (
-    <>
-      <div>
-        <button onClick={openModal}>Open Modal</button>
-        {isOpen && <Modal onClose={closeModal}></Modal>}
-      </div>
-
-      <Counter></Counter>
-      <Vote></Vote>
-      <ColorPicker></ColorPicker>
-      <TodoList></TodoList>
-    </>
+    <div className="main-box">
+      <Description></Description>
+      <Options></Options>
+      <Feedback></Feedback>
+    </div>
   );
 };
 export default App;
